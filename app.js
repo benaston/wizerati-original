4184ed7137a874167555bdc1bf45697299e089df
Ben Aston <ben@bj.ma>
2014-03-02 13:58:32 +0000

    wip

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/index2', routes.index2);
+app.get('/index3', routes.index3);
 app.get('/search', routes.index);
 app.get('/advertisers', routes.index);
 app.get('/accountactivationpanel', routes.index);
