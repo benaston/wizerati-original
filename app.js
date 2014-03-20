888aa9147ef6244b6311803375071d9487ca0ec4
ben <ben@bj.ma>
2014-03-20 22:38:56 +0000

    df

--- app.js
+++ app.js
@@ -3,36 +3,37 @@ var routes = require('./routes');
 var http = require('http');
 var path = require('path');
 var ejs = require('ejs');
+var favicon = require('static-favicon');
+var compression = require('compression');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var errorHandler = require('errorhandler');
 
 var app = express();
 
 app.disable('x-powered-by');
-//app.disable('etag');
+//app.disable('etag'); //this fixes ios safari bug at the expense of performance
 
 // all environments
 app.set('port', process.env.PORT || 3002);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.use(express.compress()); //gzip compression for HTTP responses
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-
-//ejs.open = '{{';
-//ejs.close = '}}';
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(compression()); //gzip compression for HTTP responses
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser());
+app.use(methodOverride());
 
 // development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+if ('development' === app.get('env')) {
+  app.use(errorHandler());
 }
 
 app.get('/', routes.index);
-
-app.get('/indexs', noCache, routes.indexs);
+app.get('/indexs', routes.indexs);
 app.get('/search', routes.index);
 app.get('/advertisers', routes.index);
 app.get('/accountactivationpanel', routes.index);
@@ -43,9 +44,10 @@ http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
-function noCache(req, res, next) {
-  res.header('Cache-Control', 'no-cache');
-  res.header('Expires', '-1');
-  res.header('Pragma', 'no-cache');
-  next();
-}
+//function noCache(req, res, next) {
+////  res.header('Cache-Control', 'no-cache');
+//  res.header('Cache-Control', 'public, max-age=0');
+////  res.header('Expires', '-1');
+////  res.header('Pragma', 'no-cache');
+//  next();
+//}
