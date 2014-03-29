cda30bd488f6a22b099925d0876d1588ff326046
ben <ben@bj.ma>
2014-03-29 22:26:22 +0000

    sd

--- public/javascripts/wizerati.js
+++ public/javascripts/wizerati.js
@@ -2479,7 +2479,7 @@ window.wizerati = {
             '3': './template-server/contract/',
             '4': './template-server/contractor/'
           },
-          'enableTrace': 'true'
+          'enableTrace': 'false'
         },
         prodConfig = {
           wizeratiUri: 'https://www.wizerati.com/',
