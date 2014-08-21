728e5cb70cfc4fddca74d5aa135d99d62c8f0a61
ben <ben@bj.ma>
2014-08-21 15:18:05 +0100

    DF

--- public/javascripts/wizerati.js
+++ public/javascripts/wizerati.js
@@ -2865,8 +2865,9 @@ window.wizerati = {
       var chrome30PlusOSX = /^Mozilla\/5\.0 \(Macintosh; .* Chrome\/[3-9][0-9]/g;
       var chrome30PlusWin = /^Mozilla\/5\.0 \(Windows .* Chrome\/[3-9][0-9]/g;
       var safariMavericks = /^Mozilla\/5\.0 \(Macintosh; .* Safari\/[5-9][0-9][0-9]/g;
+      var linuxAndModernAndroid = /^Mozilla\/5\.0 \(Linux; .* Safari\/[5-9][0-9][0-9]/g;
 
-      var supportedUAs  = [ iOS7iPhone, iOS7iPad, chrome30PlusOSX, chrome30PlusWin, safariMavericks ];
+      var supportedUAs  = [ iOS7iPhone, iOS7iPad, chrome30PlusOSX, chrome30PlusWin, safariMavericks, linuxAndModernAndroid ];
 
       return supportedUAs.reduce(function(previousValue, currentValue, index, array) {
           return previousValue || currentValue.test(ua);
