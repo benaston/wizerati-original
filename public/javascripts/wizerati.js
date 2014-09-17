8b1be8288b9ef3f151953e9aeb3afbb8e546bbb9
ben <ben@bj.ma>
2014-09-17 23:44:09 +0100

    fd

--- public/javascripts/wizerati.js
+++ public/javascripts/wizerati.js
@@ -2860,8 +2860,8 @@ window.wizerati = {
 
     this.isBrowserCompatible = function () {
       var ua = navigator.userAgent;
-      var iOS7iPhone = /^Mozilla\/5\.0 \(iPhone; CPU iPhone OS 7/g;
-      var iOS7iPad = /^Mozilla\/5\.0 \(iPad; CPU OS 7/g;
+      var iOS7iPhone = /^Mozilla\/5\.0 \(iPhone; CPU iPhone OS [7-9]/g;
+      var iOS7iPad = /^Mozilla\/5\.0 \(iPad; CPU OS [7-9]/g;
       var chrome30PlusOSX = /^Mozilla\/5\.0 \(Macintosh; .* Chrome\/[3-9][0-9]/g;
       var chrome30PlusWin = /^Mozilla\/5\.0 \(Windows .* Chrome\/[3-9][0-9]/g;
       var safariMavericks = /^Mozilla\/5\.0 \(Macintosh; .* Safari\/[5-9][0-9][0-9]/g;
